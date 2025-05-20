007203e15ccfd205eba874c0f63a8138bcc2afce
vikash <kumarvikashv681@gmail.com>
2025-05-21 01:09:05 +0530

    server health checked added

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth.routes");
 const productRoutes = require("./routes/product.routes");
+const healthRoutes = require("./routes/health.routes");
 const errorMiddleware = require("./middlewares/error");
 
 dotenv.config();
@@ -16,6 +17,7 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/health", healthRoutes);
 
 // Error Handling Middleware
 app.use(errorMiddleware);
